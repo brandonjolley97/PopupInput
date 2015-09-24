5e99d5379bff249feb199e6e0345d0ea4fefb928
brandonjolley97 <bjol7457@740s-pl-131.local>
2015-09-24 08:27:28 -0600

    Created a new program.
    Has Popups

---
+++ src/input/controller/InputRunner.java
@@ -0,0 +1,13 @@
+package input.controller;
+
+public class InputRunner
+{
+	public static void main (String [] args)
+	{
+		//Thing myThing = newThing;
+		//myThing.start
+		
+		PopupController myInput = new PopupController();
+		myInput.start;
+	}
+}
